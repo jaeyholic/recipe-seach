314a8dc55eebb69cac1fa3345e4276dcf3aefdbb
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-03-26 19:13:36 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+//components
+import Form from "./components/Form";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Form />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
       </div>
     );
