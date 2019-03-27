04b822d9e9716bcbed8b46b160f8721172229581
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-03-27 10:35:47 +0000

    added dynamic recipe route

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Recipes = props => (
   <div className="container">
@@ -17,12 +18,20 @@ const Recipes = props => (
                 alt={recipe.title}
               />
               <div className="recipe__text">
-                <h5 className="recipes__title">{recipe.title}</h5>
+                <h5 className="recipes__title">
+                  {recipe.title.length < 20
+                    ? `${recipe.title}`
+                    : `${recipe.title.substring(0, 23)}...`}
+                </h5>
                 <p className="recipes__subtitle">
                   Publisher: <span>{recipe.publisher}</span>
                 </p>
               </div>
-              <button className="recipe_buttons">View Recipe</button>
+              <button className="recipe_buttons">
+                <Link to={{ pathname: `/recipe/${recipe.recipe_id}` }}>
+                  View Recipe
+                </Link>
+              </button>
             </div>
           </div>
         );
