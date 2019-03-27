9eb551aa107def780b1d29bc9df5eb2b5da5bb4d
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-03-27 10:04:48 +0000

    styling recipes page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 //components
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "1763314211c22c4de226fc03af2be9fd";
 
@@ -15,7 +16,7 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=&{recipeName}&count=12`
     );
     const data = await api_call.json();
     this.setState({
@@ -30,9 +31,7 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
-        {this.state.recipes.map(recipe => (
-          <Recipe key={recipe.recipe_id} />
-        ))}
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
